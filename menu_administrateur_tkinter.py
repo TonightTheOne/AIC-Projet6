112e68b0f15656ae6e78c39eb59322a9be36cb3a
Steve Petit <steve.petit84@gmail.com>
2019-12-11 09:15:26 +0100

    GUI menu administration

--- menu_administrateur_tkinter.py
+++ menu_administrateur_tkinter.py
@@ -6,6 +6,7 @@
 # Importer les modules nécessaire pour l'exécution du script.
 from tkinter import *
 from tkinter import ttk 
+from xml.dom import minidom
 import subprocess
 import os
 
@@ -30,13 +31,25 @@ def suppression_utilisateur_linux():
 def scan_reseau_linux():
     network = input("Entrez l'IP du réseau à scanner (au format IPV4 ex: 192.168.0.0-10): ")
     os.system('xterm -hold -e nmap -PA -sV --version-light' " "  + str(network) + " " '-oX resultat-scan/rapport-scan-linux.xml')
+    os.system('grep "\(address\|ostype\)" resultat-scan/rapport-scan-linux.xml | sort -u > resultat/scan/rapport-scan-os.txt')
     os.system('grep -E -o "([0-9]{1,3}[.]){3}[0-9]{1,3}" resultat-scan/rapport-scan-linux.xml | sort -u > resultat-scan/rapport-final-linux.txt')
     print("Scan réseau et trie correctement effectué, inscription des fichier dans /resultat-scan !")
+
+def afficher_resultat_scan_complet():
+    fsock=open("resultat-scan/rapport-scan-linux.xml")
+    xmldoc = minidom.parse(fsock)
+    fsock.close()
+    print(xmldoc.toxml())
+
+def afficher_resultat_os_ip():
+    fichier_linux=open("resultat-scan/rapport-scan-os.txt", "r")
+    for line in fichier_linux:
+        print("Voici le résultat IP et OS du scan réseau Linux: ", line)
                          
-def afficher_resultat_linux():
+def afficher_resultat_ip():
     fichier_linux=open("resultat-scan/rapport-final-linux.txt", "r")
     for line in fichier_linux:
-        print("Voici le résultat trié du scan réseau Linux: ", line)
+        print("Voici le résultat trié des IP du scan réseau Linux: ", line)
 
 # Création et réglages fenêtre
 fen=Tk()
@@ -67,42 +80,52 @@ p.pack()
 # bouton pour Scan réseau linux
 scan = ttk.Button(p, text='Scan réseau', command=scan_reseau_linux)
 scan.pack(side = TOP)
-p.add(scan, minsize=75)
+p.add(scan, minsize=65)
 
-# bouton pour Afficher résultat
-trie = ttk.Button(p, text='Afficher résultat scan réseau', command=afficher_resultat_linux)
+# bouton pour Afficher résultat sans trie
+result = ttk.Button(p, text='Afficher résultat scan non trié', command=afficher_resultat_scan_complet)
+result.pack(side = TOP)
+p.add(result, minsize=65)
+
+# bouton pour Afficher résultat IP et OS
+ipos = ttk.Button(p, text='Afficher résultat IP et OS', command=afficher_resultat_os_ip)
+ipos.pack(side = TOP)
+p.add(ipos, minsize=65)
+
+# bouton pour Afficher résultat IP
+trie = ttk.Button(p, text='Afficher résultat scan IP', command=afficher_resultat_ip)
 trie.pack(side = TOP)
-p.add(trie, minsize=75)
+p.add(trie, minsize=65)
 
 # bouton pour Lister utilisateur
 listuser = ttk.Button(p, text='Lister les utilisateurs', command=list_user_linux)
 listuser.pack(side = TOP)
-p.add(listuser, minsize=75)
+p.add(listuser, minsize=65)
 
 # bouton pour Création utilisateur
 adduser = ttk.Button(p, text='Créer un nouveau utilisateur', command=creation_utilisateur_linux)
 adduser.pack(side = TOP)
-p.add(adduser, minsize=75)
+p.add(adduser, minsize=65)
 
 # bouton pour Mofidier utilisateur
 modifyuser = ttk.Button(p, text='Modifier un utilisateur', command=modification_utilisateur_linux)
 modifyuser.pack(side = TOP)
-p.add(modifyuser, minsize=75)
+p.add(modifyuser, minsize=65)
 
 # bouton pour Sauvegarde répertoire personnel utilisateur
 backuphome = ttk.Button(p, text='Sauvegarde des répertoires personnel', command=backup_home_linux)
 backuphome.pack(side = TOP)
-p.add(backuphome, minsize=75)
+p.add(backuphome, minsize=65)
 
 # bouton pour Suppression utilisateur
 deleteuser = ttk.Button(p, text='Supprimer un utilisateur', command=suppression_utilisateur_linux)
 deleteuser.pack(side = TOP)
-p.add(deleteuser, minsize=75)
+p.add(deleteuser, minsize=65)
 
 # bouton pour quitter
 quitt = ttk.Button(p, text='Quitter', command=fen.destroy)
 quitt.pack(side = TOP)
-p.add(quitt, minsize=75)
+p.add(quitt, minsize=65)
 
 # Pour terminer la boucle avec la souris ou le clavier
 fen.mainloop()
